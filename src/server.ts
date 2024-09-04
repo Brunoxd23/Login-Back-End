0f83f5f13b3a4f8cbe244dee88be4448df179c33
Brunoxd23 <Monteiro_100@outlook.pt>
2024-09-03 21:27:08 -0300

    criado arquivo .env

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import dotenv from "dotenv";
+
+// Carrega variáveis de ambiente do arquivo .env
+dotenv.config();
 
 const app = express();
 
+// Configura o middleware CORS para permitir solicitações de diferentes origens
 app.use(cors());
+
+// Configura o middleware para analisar o corpo das requisições em JSON
 app.use(express.json());
+
+// Usa o roteador importado para gerenciar rotas
 app.use(router);
 
-app.listen(3000, () =>
-  console.log("Server is running in http://localhost:3000")
-);
+// Obtém a porta a partir das variáveis de ambiente ou usa 3000 por padrão
+const PORT = process.env.PORT || 3000;
+
+// Inicia o servidor e escuta na porta especificada
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
