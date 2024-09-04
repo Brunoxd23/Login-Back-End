857e1bcf2b98880ac95c093993d23757f9c1881c
Brunoxd23 <Monteiro_100@outlook.pt>
2024-09-04 01:49:41 -0300

    git add . alteracao backend

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,8 @@ import cors from "cors";
 import { router } from "./routes";
 import dotenv from "dotenv";
 
-// Carrega variáveis de ambiente do arquivo .env apenas em ambiente de desenvolvimento
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config();
-}
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
 
 const app = express();
 
@@ -14,12 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-// Usa a porta fornecida pelo ambiente ou 3000 como fallback
+// Obter a porta a partir das variáveis de ambiente ou usar 3333 por padrão
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// Adicione um endpoint de teste
-app.get("/", (req, res) => {
-  res.json({ message: "API is working!" });
-});
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
