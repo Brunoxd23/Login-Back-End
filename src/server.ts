27bdc85680507f636b0332e3df87a967c5030dbf
Brunoxd23 <monteiro_100@outlook.pt>
2024-09-05 01:16:18 -0300

    cors atualizado

--- src/server.ts
+++ src/server.ts
@@ -7,23 +7,23 @@ dotenv.config();
 
 const app = express();
 
-// Defina as opções do CORS
+// Defina as opções de CORS para permitir o frontend acessar o backend
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "https://cronograma-provas-morato-frontend-98vb5sr0f.vercel.app", // Permite acesso do frontend
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000", // Usa a variável de ambiente CORS_ORIGIN
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
     optionsSuccessStatus: 200
 };
 
-// Use o middleware de CORS com as opções definidas
+// Aplique o middleware de CORS
 app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/api", router);
 
-// Inicializa o servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 export default app;
