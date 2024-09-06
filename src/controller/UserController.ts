6f39fd352e227399fb8461998c87ed8caec8b3c9
Bruno Monteiro <140560618+Brunoxd23@users.noreply.github.com>
2024-09-06 11:00:42 -0300

    Update UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,6 +1,8 @@
-import { prisma } from "../utils/prisma";
-import { hash } from "bcryptjs";
-import { Request, Response } from "express";
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcryptjs';
+import { Request, Response } from 'express';
+
+const prisma = new PrismaClient();
 
 export class UserController {
   async index(req: Request, res: Response) {
@@ -19,16 +21,16 @@ export class UserController {
       const userExists = await prisma.user.findUnique({ where: { email } });
 
       if (userExists) {
-        return res.status(400).json({ error: "User already exists" });
+        return res.status(400).json({ error: 'User already exists' });
       }
 
-      const hash_password = await hash(password, 8);
+      const hashedPassword = await hash(password, 8);
 
       const user = await prisma.user.create({
         data: {
           name,
           email,
-          password: hash_password,
+          password: hashedPassword,
         },
       });
       return res.status(201).json({ user });
