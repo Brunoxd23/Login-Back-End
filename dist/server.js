1f5c161688c44e22e92ba9fb56b83edc81ee239d
Brunoxd23 <Monteiro_100@outlook.pt>
2024-09-06 12:54:19 -0300

    server.js

--- dist/server.js
+++ dist/server.js
@@ -9,26 +9,17 @@ const routes_1 = require("./routes");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const allowedOrigins = [
-    process.env.CORS_ORIGIN,
-    "https://login-back-end.vercel.app/",
-    "https://login-back-612ma7gek-brunos-projects-e6b83a53.vercel.app"
-];
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    optionsSuccessStatus: 200,
-    credentials: true
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    optionsSuccessStatus: 200
 };
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use("/api", routes_1.router);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+// Apenas para desenvolvimento local
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+}
+// Exportar para uso serverless
 exports.default = app;
