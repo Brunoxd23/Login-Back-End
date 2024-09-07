6afa36792a9d0f886876234e8ea84574cddf7733
Brunoxd23 <Monteiro_100@outlook.pt>
2024-09-07 01:00:18 -0300

    alteracao

---
+++ src/server.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || "https://cronograma-provas-morato-frontend-leobf9xxx.vercel.app/",
+    optionsSuccessStatus: 200
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.use("/api", routes_1.router);
+// Apenas para desenvolvimento local
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+}
+// Exportar para uso serverless
+exports.default = app;
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "https://login-back-end.vercel.app/",
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use("/api", router);
+app.use(router);
 
-// Apenas para desenvolvimento local
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () =>
-    console.log(`Server is running on http://localhost:${PORT}`)
-  );
-}
-
-// Exportar para uso serverless
-export default app;
+app.listen(3000, () =>
+  console.log("Server is running in http://localhost:3000")
+);
