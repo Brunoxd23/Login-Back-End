295d26dd284db95f63272b8b9141b64be8ee52ff
Brunoxd23 <monteiro_100@outlook.pt>
2024-09-06 22:26:34 -0300

    server atualizado

--- dist/server.js
+++ dist/server.js
@@ -10,12 +10,14 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "https://login-back-end.vercel.app/", // Permite acesso da URL do frontend
     optionsSuccessStatus: 200
 };
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use("/api", routes_1.router);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 // Apenas para desenvolvimento local
 if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 3000;
