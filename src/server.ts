330dd23b8e1e2b63be5fa8e3febf6977a7fc8e85
Brunoxd23 <monteiro_100@outlook.pt>
2024-09-06 22:28:47 -0300

    resolvendo conflitos

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors";
-import { router } from "./routes";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import { router } from './routes';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "https://login-back-end.vercel.app/", // Permite acesso da URL do frontend
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000", // Permite acesso da URL do frontend
     optionsSuccessStatus: 200
 };
 
@@ -21,13 +21,4 @@ app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
 
-// Apenas para desenvolvimento local
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () =>
-    console.log(`Server is running on http://localhost:${PORT}`)
-  );
-}
-
-// Exportar para uso serverless
 export default app;
