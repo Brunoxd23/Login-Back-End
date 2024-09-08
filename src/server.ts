2e4150d0d792f68d1716c77e04c4dabe2aafb0f7
Brunoxd23 <monteiro_100@outlook.pt>
2024-09-08 02:53:48 -0300

    server req

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Middleware de log (opcional)
-app.use((req, res, next) => {
+app.use((req, _res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 app.use('/api', router);
 
 // Rota de teste
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.json({ message: 'Backend is running' });
 });
 
