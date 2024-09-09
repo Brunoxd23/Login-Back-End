1aff31caaa86600e3cfe95d6da1d3a41fed40fc4
Brunoxd23 <Monteiro_100@outlook.pt>
2024-09-09 12:21:28 -0300

    resolvendo conflitos

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { router } from './routes';
-
-dotenv.config();
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
 
 const app = express();
 
 const allowedOrigins = [
-  'https://cronograma-provas-morato-frontend.vercel.app',
-  'http://localhost:3000'
+  "https://cronograma-provas-morato-frontend.vercel.app",
+  "http://localhost:3000"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+  })
+);
 
 app.use(express.json());
 
 // Handle preflight requests
-app.options('*', cors());
+app.options("*", cors());
 
-app.use('/api', router);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 
